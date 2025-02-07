f68a47363b542a3dc05934625c2881df5cb510de
ryul00 <gju07059@gmail.com>
2025-02-07 17:40:07 +0900

    최종 커밋 ㅎㅎ

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ActivityPage from './Components/ActivityPage'
 import BabyLionPage from './Components/BabyLionPage'
 import ReviewPage from './Components/ReviewPage';
-import './main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<div id='subroot'><ActivityPage /><BabyLionPage /><ReviewPage /></div>} />
+        <Route path="/" element={<ReviewPage />} />
+        <Route path="/Activity" element={<ActivityPage />} />
+        <Route path="/BabyLion" element={<BabyLionPage />} />
       </Routes>
     </Router>
   </StrictMode>
