177f8f855d3bb665790dfcefbc6066c095dfbe97
ryul00 <gju07059@gmail.com>
2025-02-07 17:37:21 +0900

    합치다가 망함

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import ActivityPage from './RecruitPage2/ActivityPage'
-import BabyLionPage from './RecruitPage2/BabyLionPage'
-import ReviewPage from './RecruitPage2/ReviewPage';
+import ActivityPage from './Components/ActivityPage'
+import BabyLionPage from './Components/BabyLionPage'
+import ReviewPage from './Components/ReviewPage';
+import './main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<ReviewPage />} />
-        <Route path="/Activity" element={<ActivityPage />} />
-        <Route path="/BabyLion" element={<BabyLionPage />} />
+        <Route path="/" element={<div id='subroot'><ActivityPage /><BabyLionPage /><ReviewPage /></div>} />
       </Routes>
     </Router>
   </StrictMode>
