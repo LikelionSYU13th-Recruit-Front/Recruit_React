487ae0c30b00333bee64cecd7f6830287f2be5bb
No4h <no4h1209@gmail.com>
2025-02-07 19:42:05 +0900

    1차 머지 완료

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,26 @@ import RecruitMain from './Components/RecruitMain.jsx'
 import RecruitAbout from './Components/RecruitAbout.jsx';
 import RecruitTrackIntro from './Components/RecruitTrackIntro.jsx';
 
+// 광렬이형
+import ActivityPage from './Components/ActivityPage'
+import BabyLionPage from './Components/BabyLionPage'
+import ReviewPage from './Components/ReviewPage';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<div id='subroot'><Header /><RecruitMain /><RecruitAbout /><RecruitTrackIntro /></div>} />
+        <Route path="/" element={<div id='subroot'><Header /><RecruitMain /><RecruitAbout /><RecruitTrackIntro /><ActivityPage /></div>} />
+
+      </Routes>
+    </Router>
+  </React.StrictMode>
+)
+ReactDOM.createRoot(document.getElementById('root2')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div id='subroot'><BabyLionPage /><ReviewPage /></div>} />
 
       </Routes>
     </Router>
