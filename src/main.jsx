f45212f18d0de2ddad668a9ea87696881d9fec20
heeyoung00 <ils09019@naver.com>
2025-02-09 18:09:05 +0900

    푸터완성성

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import QNA from './ScheduleQNA/QNA.JSX'
 import Schedule from './ScheduleQNA/Schedule.jsx'
+import Footer from "./ScheduleQNA/Footer.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Schedule />
     <QNA />
+    <Footer />
   </StrictMode>,
 )
